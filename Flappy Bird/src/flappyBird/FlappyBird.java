610d284af70c2e4427e3fbcf904510ed3c0c29fb
Omer Atmon <omeratz@gmail.com>
2016-08-29 00:06:48 +0300

    ordered

--- Flappy Bird/src/flappyBird/FlappyBird.java
+++ Flappy Bird/src/flappyBird/FlappyBird.java
@@ -59,9 +59,6 @@ public class FlappyBird implements ActionListener, MouseListener{
 		timer.start();
 	}
 	
-	public static void main(String[] args) {
-		flappyBird = new FlappyBird();
-	}
 	
 	public void addColumn(boolean start){
 		int space = 300;
@@ -163,30 +160,6 @@ public class FlappyBird implements ActionListener, MouseListener{
 		}
 		renderer.repaint();
 	}
-	
-	public void jump(){
-		if (gameOver){
-			bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20 ,20);
-			columns.clear();
-			yMotion = 0;
-			score = 0;
-			
-			addColumn(true);
-			addColumn(true);
-			addColumn(true);
-			addColumn(true);
-			
-			gameOver = false;			
-		}
-		
-		if (!started){
-			started = true;
-		} else if (!gameOver) {
-			if (yMotion > 0)
-				yMotion = 0;
-			yMotion -= 10;
-		}
-	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -211,6 +184,34 @@ public class FlappyBird implements ActionListener, MouseListener{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		
+	}
+	
+	public void jump(){
+		if (gameOver){
+			bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20 ,20);
+			columns.clear();
+			yMotion = 0;
+			score = 0;
+			
+			addColumn(true);
+			addColumn(true);
+			addColumn(true);
+			addColumn(true);
+			
+			gameOver = false;			
+		}
+		
+		if (!started){
+			started = true;
+		} else if (!gameOver) {
+			if (yMotion > 0)
+				yMotion = 0;
+			yMotion -= 10;
+		}
+	}
+	
+	public static void main(String[] args) {
+		flappyBird = new FlappyBird();
 	}
 
 }
