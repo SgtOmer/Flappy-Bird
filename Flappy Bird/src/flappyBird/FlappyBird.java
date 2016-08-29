acb9bd2f0f0eec27ed99b390e461a4ff7d58b305
Omer Atmon <omeratz@gmail.com>
2016-08-29 18:18:23 +0300

    order

--- Flappy Bird/src/flappyBird/FlappyBird.java
+++ Flappy Bird/src/flappyBird/FlappyBird.java
@@ -7,6 +7,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -15,7 +17,7 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener, MouseListener{
+public class FlappyBird implements ActionListener, MouseListener, KeyListener{
 
 	public static FlappyBird flappyBird;
 	
@@ -45,6 +47,7 @@ public class FlappyBird implements ActionListener, MouseListener{
 		jframe.setTitle("Flappy Bird");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.addMouseListener(this);
+		jframe.addKeyListener(this);
 		jframe.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		jframe.setResizable(false);
 		jframe.pack();
@@ -210,6 +213,24 @@ public class FlappyBird implements ActionListener, MouseListener{
 				yMotion = 0;
 			yMotion -= 10;
 		}
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_SPACE)
+			jump();
+	}
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
 	}
 	
 	public static void main(String[] args) {
